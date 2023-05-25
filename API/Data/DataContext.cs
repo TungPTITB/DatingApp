2c6f48f3c977d38f9dbc4f87576badd23860f2d4
TungPTITB <94946324+TungPTITB@users.noreply.github.com>
2023-05-25 16:32:37 +0700

    init

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Thing
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options){
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
